ac9b5bf8724fcf4e2767896bade480a44120e02b
Ghanshyamsinh Zala <130747789+GZ30eee@users.noreply.github.com>
2025-02-04 15:41:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ pip install --upgrade pip
 # Install required Python packages
 pip install -r requirements.txt
 
-# Force download and install spaCy model
+# Install spaCy model (en_core_web_sm)
 python -m spacy download en_core_web_sm
 
-# Explicitly link the model in case it's not found
+# Ensure model is linked in case of any issues
 python -c "import spacy; spacy.load('en_core_web_sm')"
